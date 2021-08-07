28c16bcaaa4a03455fab1416c55382fdc9e68b41
Duncan Brown <iamduncan@users.noreply.github.com>
2021-08-07 21:19:04 +0000

    :bug: modules are now loaded correctly

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Author } from './entities/author.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Author])],
   controllers: [AuthorsController],
   providers: [AuthorsService],
 })
--- src/plugins/plugins.module.ts
+++ src/plugins/plugins.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PluginsService } from './plugins.service';
 import { PluginsController } from './plugins.controller';
+import { Plugin } from './entities/plugin.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Plugin])],
   controllers: [PluginsController],
   providers: [PluginsService],
 })
