413cad69b8cd822bb3b6f55b4ff9b087f2e68e51
Duncan Brown <iamduncan@users.noreply.github.com>
2021-08-11 19:08:59 +0000

    🩹 (app) root path now shows a better message

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { message: string } {
+    return {
+      message:
+        'Welcome to the API!,you can find all the endpoints here: https://github.com/wessex-digital-solutions/wordpress-update-server/wiki',
+    };
   }
 }
